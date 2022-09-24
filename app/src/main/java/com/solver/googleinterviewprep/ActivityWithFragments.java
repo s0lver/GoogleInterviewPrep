b9dc6eaf4f16285818ddd139c0b874906ad1b5c9
s0lver <rperez.github@gmail.com>
2022-09-24 15:46:37 -0500

    Add animations

--- app/src/main/java/com/solver/googleinterviewprep/ActivityWithFragments.java
+++ app/src/main/java/com/solver/googleinterviewprep/ActivityWithFragments.java
@@ -4,33 +4,55 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class ActivityWithFragments extends AppCompatActivity {
     // private FragmentContainerView fragView;
     private SampleFragment fragment1;
     private SampleFragment2 fragment2;
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_with_fragments);
-
-        // this.fragView = findViewById(R.id.fragmentContainerView);
     }
 
     public void clickOnViewFrag1(View view) {
         if (this.fragment1 == null) {
             this.fragment1 = new SampleFragment();
         }
-        getSupportFragmentManager().beginTransaction()
-            .replace(R.id.fragmentContainerView, this.fragment1).commitNow();
+
+        if (this.currentFragment != this.fragment1) {
+
+            getSupportFragmentManager().beginTransaction()
+                .setCustomAnimations(
+                    R.anim.slide_in,  // enter
+                    R.anim.fade_out,  // exit
+                    R.anim.fade_in,   // popEnter
+                    R.anim.slide_out  // popExit
+                )
+                .replace(R.id.fragmentContainerView, this.fragment1).commitNow();
+            this.currentFragment = this.fragment1;
+        }
     }
 
     public void clickOnViewFrag2(View view) {
         if (this.fragment2 == null) {
             this.fragment2 = new SampleFragment2();
         }
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView,
-            this.fragment2).commitNow();
+
+        if (this.currentFragment != this.fragment2) {
+            getSupportFragmentManager().beginTransaction()
+                .setCustomAnimations(
+                    R.anim.slide_in,  // enter
+                    R.anim.fade_out,  // exit
+                    R.anim.fade_in,   // popEnter
+                    R.anim.slide_out  // popExit
+                ).replace(R.id.fragmentContainerView, this.fragment2).commitNow();
+            this.currentFragment = this.fragment2;
+        }
+
+
     }
 }
