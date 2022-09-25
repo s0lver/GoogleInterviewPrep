dab2d3af043c77caf618c1ff53e3e1f4f85e5e32
s0lver <rperez.github@gmail.com>
2022-09-24 23:26:41 -0500

    Add worker example

--- app/src/main/java/com/solver/googleinterviewprep/SampleFragment2.java
+++ app/src/main/java/com/solver/googleinterviewprep/SampleFragment2.java
@@ -1,5 +1,8 @@
 package com.solver.googleinterviewprep;
 
+import static com.solver.googleinterviewprep.Utils.createUrlString;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,8 +12,20 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
 
 import com.google.android.material.snackbar.Snackbar;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class SampleFragment2 extends Fragment {
@@ -25,7 +40,7 @@ public class SampleFragment2 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+        Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_sample2, container, false);
     }
 
@@ -37,6 +52,44 @@ public class SampleFragment2 extends Fragment {
     }
 
     public void onDoMagicButtonClick(View view) {
-        Snackbar.make(view, "Doing magic in fragment 2", Snackbar.LENGTH_LONG).show();
+        WorkRequest request = new OneTimeWorkRequest.Builder(WorkTask.class).build();
+
+        WorkManager workerManagerInstance = WorkManager.getInstance(this.getActivity());
+
+        workerManagerInstance.enqueue(request);
+
+        workerManagerInstance.getWorkInfoByIdLiveData(request.getId()).observe(this, workInfo -> {
+            if (workInfo != null && workInfo.getState().isFinished()) {
+                Data outputData = workInfo.getOutputData();
+
+                Snackbar.make(view, outputData.getString("body"), Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public static class WorkTask extends Worker {
+
+        public WorkTask(@NonNull Context context,
+            @NonNull WorkerParameters workerParams) {
+            super(context, workerParams);
+        }
+
+        @NonNull
+        @Override
+        public Result doWork() {
+            Request request = new Request.Builder().url(createUrlString()).build();
+            OkHttpClient client = new OkHttpClient();
+            try {
+                Response response = client.newCall(request).execute();
+                if (response.body() != null) {
+                    return Result.success(new Data.Builder().putString("body", response.body()
+                        .string()).build());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return Result.failure(new Data.Builder().putString("body",
+                "Somebody failed and it was not me").build());
+        }
     }
 }
